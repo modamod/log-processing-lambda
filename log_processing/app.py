551a33ae027ddaed541cec578fbd9ff202ad6c7c
modamod <modhaffer.rahmani@gmail.com>
2021-06-29 18:04:31 +0100

    First attempt to process logs from cloudwatch using lambda

---
+++ log_processing/app.py
@@ -0,0 +1,41 @@
+import json
+import base64
+import gzip
+
+
+# import requests
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        Cloudwatch log subscription event
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+        The event that was recieved.
+
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+    cw_data = event['awslogs']['data']
+    compressed_payload = base64.b64decode(cw_data)
+    uncompressed_payload = gzip.decompress(compressed_payload)
+    payload = json.loads(uncompressed_payload)
+
+    print(payload)
+    return payload
